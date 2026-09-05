0178ba5ea128504ee501a8129cfd719a80517343
Devendra Verma <dev2505verma@gmail.com>
2026-09-05 20:15:10 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/com/devendraverma/ai_app_builder/AiAppBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devendraverma.ai_app_builder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiAppBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
